1622b3b2adadc8a5a69071574d9d07490dab3337
zzq <i@zhangzqs.cn>
2025-01-21 00:37:39 +0800

    添加一些resty日志

--- compcont-resty/simple_resty.go
+++ compcont-resty/simple_resty.go
@@ -6,8 +6,10 @@ import (
 	"sync"
 
 	"github.com/go-compcont/compcont-core"
+	compcontzap "github.com/go-compcont/compcont-std/compcont-zap"
 	"github.com/go-compcont/compcont-std/reloading"
 	"github.com/go-resty/resty/v2"
+	"go.uber.org/zap"
 )
 
 func statusCodeHitTest(statusCodeCfg []int, code int) bool {
@@ -65,16 +67,39 @@ func (c *simpleProviderImpl) getRestyNoOnce() (cli *resty.Client, err error) {
 		SetRetryWaitTime(*c.Retry.WaitTime).
 		SetRetryCount(*c.Retry.MaxCount).
 		OnBeforeRequest(func(cli *resty.Client, r *resty.Request) error {
+			logger := compcontzap.FromContext(r.Context())
 			if c.userAgentProvider != nil {
 				userAgentList, err := c.userAgentProvider.LoadConfig(r.Context())
 				if err != nil {
+					logger.Error("load user-agent config error", zap.Error(err))
 					return err
 				}
 				if len(userAgentList) > 0 {
 					userAgent := userAgentList[rand.N(len(userAgentList))]
 					r.Header.Set("User-Agent", userAgent)
+					logger.Info("request with User-Agent", zap.String("userAgent", userAgent))
 				}
 			}
+			logger.Debug(
+				"resty request",
+				zap.Int("attempt", r.Attempt),
+				zap.String("method", r.Method),
+				zap.String("url", r.URL),
+				zap.Any("header", r.Header),
+			)
+			return nil
+		}).
+		OnAfterResponse(func(c *resty.Client, r *resty.Response) error {
+			logger := compcontzap.FromContext(r.Request.Context())
+			logger.Debug(
+				"resty response",
+				zap.Int("attempt", r.Request.Attempt),
+				zap.String("status", r.Status()),
+				zap.Int("statusCode", r.StatusCode()),
+				zap.Any("header", r.Header),
+				zap.Any("bodyString", r.String()),
+				zap.Duration("time", r.Time()),
+			)
 			return nil
 		})
 	if c.Proxy.Enabled {
@@ -84,10 +109,13 @@ func (c *simpleProviderImpl) getRestyNoOnce() (cli *resty.Client, err error) {
 	}
 	for _, retryCondition := range c.SimpleProviderConfig.Retry.Condition {
 		cli.AddRetryCondition(func(r *resty.Response, err error) bool {
+			logger := compcontzap.FromContext(r.Request.Context())
 			if err != nil {
+				logger.Error("retry error", zap.Error(err))
 				return true
 			}
 			if statusCodeHitTest(retryCondition.StatusCode, r.StatusCode()) {
+				logger.Warn("retry response by status code", zap.Int("statusCode", r.StatusCode()))
 				return true
 			}
 			return false
