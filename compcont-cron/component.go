0573674f6307e5a40159ad2121eeda819d333c5a
zzq <i@zhangzqs.cn>
2025-02-15 16:21:46 +0800

    cron组件实现手工调度

--- compcont-cron/component.go
+++ compcont-cron/component.go
@@ -1,24 +1,54 @@
 package compcontcron
 
 import (
+	"context"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 	"github.com/go-compcont/compcont-core"
+	"go.uber.org/zap"
 )
 
 const TypeID compcont.ComponentTypeID = "contrib.simple-cron-scheduler"
 
 type Component interface {
-	AddTask(taskName string, fn func())
+	AddTask(taskName string, fn func(ctx context.Context) error)
+	DoTask(ctx context.Context, taskName string) error
 }
 
 type Config struct {
-	Enabled bool                `ccf:"enabled"` // 是否启用调度
-	Policy  map[string][]string `ccf:"policy"`  // 各个定时任务调度策略map[taskName]crontab
+	Logger      *compcont.TypedComponentConfig[any, *zap.Logger] `ccf:"logger"`        // 任务日志
+	Enabled     bool                                             `ccf:"enabled"`       // 是否启用自动调度
+	Policy      map[string][]string                              `ccf:"policy"`        // 各个定时任务调度策略map[taskName]crontab
+	Gin         *compcont.TypedComponentConfig[any, gin.IRouter] `ccf:"gin"`           // 管理接口
+	GinDoRouter string                                           `ccf:"gin_do_router"` // 任务执行接口
 }
 
 var factory compcont.IComponentFactory = &compcont.TypedSimpleComponentFactory[Config, Component]{
 	TypeID: TypeID,
 	CreateInstanceFunc: func(ctx compcont.BuildContext, config Config) (instance Component, err error) {
-		instance = New(config)
+		var logger *zap.Logger
+		if config.Logger == nil {
+			logger, err = zap.NewDevelopment()
+			if err != nil {
+				return
+			}
+		} else {
+			logger = config.Logger.MustLoadComponent(ctx.Container).Instance
+		}
+		instance = New(logger, config)
+		if config.Gin != nil {
+			ginComp := config.Gin.MustLoadComponent(ctx.Container).Instance
+			ginComp.GET(config.GinDoRouter, func(ctx *gin.Context) {
+				err := instance.DoTask(ctx, ctx.Query("task_name"))
+				if err != nil {
+					ctx.JSON(http.StatusInternalServerError, gin.H{
+						"error": err.Error(),
+					})
+					return
+				}
+			})
+		}
 		return
 	},
 }
--- compcont-cron/cron.go
+++ compcont-cron/cron.go
@@ -1,24 +1,78 @@
 package compcontcron
 
-import "github.com/robfig/cron/v3"
+import (
+	"context"
+	"errors"
+	"fmt"
+	"time"
+
+	compcontzap "github.com/go-compcont/compcont-std/compcont-zap"
+	"github.com/robfig/cron/v3"
+	"go.uber.org/zap"
+)
 
 type componentImpl struct {
 	cron   *cron.Cron
 	policy map[string][]string
+	fnMap  map[string]func(ctx context.Context) error
+	logger *zap.Logger
 }
 
-func New(cfg Config) *componentImpl {
+func New(logger *zap.Logger, cfg Config) *componentImpl {
 	c := cron.New()
 	if cfg.Enabled {
 		c.Start()
 	}
-	return &componentImpl{cron: c, policy: cfg.Policy}
+	return &componentImpl{
+		logger: logger,
+		cron:   c,
+		policy: cfg.Policy,
+		fnMap:  make(map[string]func(ctx context.Context) error),
+	}
 }
 
-func (c *componentImpl) AddTask(taskName string, fn func()) {
+func (c *componentImpl) DoTask(ctx context.Context, taskName string) (err error) {
+	fmt.Println(c.fnMap)
+	if fn, ok := c.fnMap[taskName]; !ok {
+		return errors.New("task not found: " + taskName)
+	} else {
+		logger := c.logger.With(
+			zap.String("taskName", taskName),
+			zap.String("taskStartTime", time.Now().Format(time.RFC3339)),
+			zap.String("taskPolicy", "manual"),
+		)
+
+		logger.Info("do task")
+		if err = fn(ctx); err != nil {
+			logger.Error("task run failed", zap.Error(err))
+			return
+		}
+		logger.Info("do task finished", zap.String("taskEndTime", time.Now().Format(time.RFC3339)))
+	}
+	return
+}
+
+func (c *componentImpl) AddTask(taskName string, fn func(ctx context.Context) error) {
+	c.logger.Info("AddTask", zap.String("taskName", taskName))
+	c.fnMap[taskName] = fn
 	if taskPolicy, ok := c.policy[taskName]; ok {
 		for _, policy := range taskPolicy {
-			c.cron.AddFunc(policy, fn)
+			if policy == "manual" {
+				continue
+			}
+			c.cron.AddFunc(policy, func() {
+				logger := c.logger.With(
+					zap.String("taskName", taskName),
+					zap.String("taskStartTime", time.Now().Format(time.RFC3339)),
+					zap.String("taskPolicy", policy),
+				)
+				err := fn(compcontzap.WithContext(context.Background(), logger))
+				if err != nil {
+					logger.Error("task run failed", zap.Error(err))
+					return
+				}
+				logger.Info("do task finished", zap.String("taskEndTime", time.Now().Format(time.RFC3339)))
+			})
 		}
 		return
 	}
